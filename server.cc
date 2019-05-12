c5f5d9f0cfd16979b03bb16b033e497c38831859
flamingcow <flamingcow@gmail.com>
2019-05-11 21:08:32 -0700

    Spec compliance (Accepts not required, no caching, charset, no buffering)

--- server.cc
+++ server.cc
@@ -6,23 +6,16 @@ Server::Server(int port, const std::function<void(std::unique_ptr<Stream>)>& cal
 		: callback_(callback),
 		  firecgi_server_(port,
 		                  [this](firecgi::Request* request) { OnRequest(request); },
-						  1,
-						  {"HTTP_ACCEPT"}) {}
+						  1) {}
 
 void Server::Serve() {
 	firecgi_server_.Serve();
 }
 
 void Server::OnRequest(firecgi::Request* request) {
-	if (request->GetParam("HTTP_ACCEPT") != "text/event-stream") {
-		LOG(WARNING) << "bad HTTP_ACCEPT: " << request->GetParam("HTTP_ACCEPT");
-		request->WriteHeader("Status", "400 Bad Request");
-		request->WriteHeader("Content-Type", "text-plain");
-		request->WriteBody("No \"Accept: text/event-stream\" header found in request. Please call this endpoint using EventSource.");
-		request->End();
-		return;
-	}
-	request->WriteHeader("Content-Type", "text/event-stream");
+	request->WriteHeader("Content-Type", "text/event-stream; charset=utf-8");
+	request->WriteHeader("Cache-Control", "no-cache");
+	request->WriteHeader("X-Accel-Buffering", "no");
 	request->WriteBody("");
 	callback_(std::make_unique<Stream>(request));
 }
